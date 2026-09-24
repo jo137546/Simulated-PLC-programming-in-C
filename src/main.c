f0b0750fc21314f23a65feb390bc888d3d80f040
Emma <153746007+jo137546@users.noreply.github.com>
2026-09-24 20:49:43 +0000

    Add initial PLC main source file

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    bool startInput = true;
+    bool stopInput = false;
+    bool estopInput = false;
+
+    bool motorOutput = false;
+    bool alarmOutput = false;
+
+    return 0;
+} // plc for tuning later
